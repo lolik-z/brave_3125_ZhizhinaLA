52e822726bf6071c9b587b686e5acdbc5b007e10
lolik-z <lolik_z@mail.ru>
2022-09-01 22:29:25 +0300

    added Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Введите целое число ");
+try
+    {   
+    int x=Convert.ToInt32 (Console.ReadLine());
+    if (x%2==0)
+        {
+            Console.WriteLine("Вы ввели четное число");
+        }
+    else Console.WriteLine("Вы ввели нечетное число");
+    }
+catch 
+    {            
+        Console.WriteLine("Что-то пошло не так!");
+    }
