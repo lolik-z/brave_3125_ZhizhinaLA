27b2cd96474eec7f3fcfecb6565f3d734b279d9c
lolik-z <lolik_z@mail.ru>
2022-09-01 22:17:05 +0300

    added Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write ("Введите целое число ");
+try
+    {   
+    int x=Convert.ToInt32 (Console.ReadLine());
+    Console.Write ("Введите еще одно целое число ");
+    int y=Convert.ToInt32 (Console.ReadLine());
+    Console.Write ("Bведите третье целое число ");
+    int z=Convert.ToInt32 (Console.ReadLine());
+    int max=x;
+    if (y>max)
+        {
+            max=y;
+        }
+    if (z>max) 
+        {
+            max=z;
+        }
+    else Console.WriteLine("Максимальное из этих чисел "+max);
+    }
+catch 
+    {            
+        Console.WriteLine("Что-то пошло не так!");
+    }
