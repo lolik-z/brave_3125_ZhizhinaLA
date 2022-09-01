6ad3be8a60c25e26060474d4f7f1f0342cde404b
lolik-z <lolik_z@mail.ru>
2022-09-01 21:53:25 +0300

    added Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write ("Введите целое число ");
+try
+    {   
+    int x=Convert.ToInt32 (Console.ReadLine());
+    Console.Write ("Введите еще одно целое число ");
+    int y=Convert.ToInt32 (Console.ReadLine());
+    if (x>y)
+        {
+        Console.WriteLine("Максимальное из двух чисел "+x+", а минимальное "+y);
+        }
+    else if (y>x) Console.WriteLine("Максимальное из двух чисел "+y+", а минимальное "+x);
+    else Console.WriteLine("Вы ввели равные числа "+x+"="+y);
+    }
+catch 
+    {
+                    
+        Console.WriteLine("Что-то пошло не так!");
+    }
