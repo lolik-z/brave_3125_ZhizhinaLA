ec49151b57b20cb3f1c2cecc8563a3a8c889502a
lolik-z <lolik_z@mail.ru>
2022-09-01 22:53:15 +0300

    added Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write ("Введите целое положительное число больше единицы ");
+try
+    {   
+    int x=Convert.ToInt32 (Console.ReadLine());
+    int i = 2;
+    
+    if (x<2)
+        {
+            Console.WriteLine("Вы ввели неподходящее число");
+        }
+        else
+        {
+            Console.WriteLine("Чётные числа от 1 до "+x+": ");
+            while (i<=x)
+                {
+                Console.Write (+i+", ");
+                i=i+2;
+                }
+        }
+    }
+catch 
+    {            
+        Console.WriteLine("Что-то пошло не так!");
+    }
