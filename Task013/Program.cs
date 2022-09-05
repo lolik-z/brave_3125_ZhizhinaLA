84890bbe5623ebe0ed62b46bd51e16469ab2cb8d
lolik-z <lolik_z@mail.ru>
2022-09-05 17:27:38 +0300

    added Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+void numbers(int num)
+{
+    int temp=num;
+    int coun=0;
+    int coun1=0;
+    while (temp > 0)
+            {
+                temp = temp / 10;
+                coun++;
+            }
+    if (coun < 3)
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+    else
+    {
+       // Console.WriteLine($"Разрядность числа {coun}");
+        temp=num;
+        while (coun1 < coun-3)
+        {
+            temp = temp / 10;
+            coun1++;
+        }
+            Console.WriteLine($"Третья цифра введенного числа {temp%10}");
+    }
+}
+
+Console.Write ("Введите число ");
+int num=Convert.ToInt32 (Console.ReadLine());
+
+numbers(num);
