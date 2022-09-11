97109552db42fad04ad0d6d8082a6d1bc9962fe0
lolik-z <lolik_z@mail.ru>
2022-09-11 19:28:52 +0300

    added Task27

---
+++ Task027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int SumN(int x)
+{
+    int result = 0;
+    int temp=x;
+    for (int i = 0; i < x; i++) 
+    {
+        result = result + temp%10;
+        temp = temp / 10;
+    }
+    return result;
+}
+Console.Write("Введите целое положительное число ");
+try
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Cумма цифр в числе {x} -> {SumN(x)}");
+}
+catch
+{
+    Console.WriteLine("Что-то пошло не так!");
+}
