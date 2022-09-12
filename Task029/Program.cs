9082b20a7859cbc3d0da96c6ab2c00d9b8af20da
lolik-z <lolik_z@mail.ru>
2022-09-12 15:18:57 +0300

    added Task29

---
+++ Task029/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран. 
+// N - вводится с клавиатуры. Элементы тоже вводятся с клавиатуры.
+// 5
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 3
+// 6, 1, 33 -> [6, 1, 33]
+int[] ArrayN(int N)
+{
+    int[] array = new int[N];
+    Console.WriteLine("Введите значения элементов массива ");
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+try
+{
+    Console.Write("Введите целое число ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int[] array = ArrayN(a);
+    Console.Write("Задан массив [");
+    for (int i = 0; i < a - 1; i++) Console.Write(array[i] + ", ");
+    Console.Write(array[a - 1] + "]");
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Ошибка ввода!");
+}
